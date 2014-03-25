6ebb41cec604af6eb2150981c844693eaee4d450
Shehmeer Jiwani <shehmeer.jiwani@gmail.com>
2014-03-25 04:43:25 -0400

    added Register

--- src/actions.sql
+++ src/actions.sql
@@ -8,3 +8,11 @@ in the Information Flow Diagram.
 SELECT password
 FROM admin,doctor,patient
 WHERE username=$username;
+
+--Register (as Doctor)
+INSERT INTO doctor (username, password) --rest of entry is blank until profile is filled out
+VALUES ($username, $password);
+
+--Register (as Patient)
+INSERT INTO patient (username, password) --again, blank until profile is updated
+VALUES ($username, password);
