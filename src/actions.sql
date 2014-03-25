0206e8d15fdf37767cbac836a2d8499f886910f7
Shehmeer Jiwani <shehmeer.jiwani@gmail.com>
2014-03-25 02:08:24 -0400

    Create actions.sql (+ Log In query)

---
+++ src/actions.sql
@@ -0,0 +1,10 @@
+/*
+This is the list of all SQL queries
+needed to replicate the actions specified
+in the Information Flow Diagram.
+*/
+
+# Log In
+SELECT password
+FROM admin, doctor, patient
+WHERE username=$username
