07364aa1d869aa2f83e22ed56ce331c62d382e6b
Shehmeer Jiwani <shehmeer.jiwani@gmail.com>
2014-03-25 04:54:57 -0400

    added Doctor and Patient Profiles

--- src/actions.sql
+++ src/actions.sql
@@ -16,3 +16,30 @@ VALUES ($username, $password);
 --Register (as Patient)
 INSERT INTO patient (username, password) --again, blank until profile is updated
 VALUES ($username, password);
+
+--Create (or update) Doctor Profile
+UPDATE doctor
+SET license_no=$license_no
+    , first_name=$first_name
+    , last_name=$last_name
+    , date_of_birth=$date_of_birth
+    , work_phone=$work_phone
+    , specialty=$specialty
+    , room_no=$room_no
+    , home_address=$home_address
+WHERE username=$current_username
+
+--Create (or update) Patient Profile
+UPDATE patient
+SET name=$name
+    , phone=$phone
+    , date_of_birth=$date_of_birth
+    , gender=$gender
+    , address=$address
+    , annual_income=$annual_income
+    , emergency_contact_name=$emergency_contact_name
+    , emergency_contact_phone=$emergency_contact_phone
+    , height=$height
+    , weight=$weight
+    , card_no=$card_no
+WHERE username=$current_username
